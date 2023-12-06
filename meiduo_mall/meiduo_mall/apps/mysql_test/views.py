bed72f19b2aa3aaaa55533e3a0ddba6a3f44b06f
zhangsan <2356470028@qq.com>
2023-12-07 01:46:38 +0800

    20231207

--- meiduo_mall/meiduo_mall/apps/mysql_test/views.py
+++ meiduo_mall/meiduo_mall/apps/mysql_test/views.py
@@ -1,6 +1,8 @@
+from django.core import serializers
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views import View
+import json
 # Create your views here.
 
 
@@ -9,16 +11,19 @@ from mysql_test.models import UserTest
 
 def query_by_id(request):
     """
-        根据id进行查询
+        查询第一个值
     :param request:
     :param id: uid
     :return:
     """
 
-    my_object = UserTest.objects.all()
-    for obj in my_object:
-        print(obj)
+    # my_object = UserTest.objects.all()
+    # for obj in my_object:
+    #     print(type(obj),obj)
+
+    the_first_data = UserTest.objects.first()
+    serializers.serialize("json")
 
     # return HttpResponse('ok')
 
-    return JsonResponse({'code': 0, 'errmsg': 'RETCODE.ok', 'id': obj})
+    return JsonResponse({'code': 0, 'errmsg': 'RETCODE.ok', 'id': the_first_data})
